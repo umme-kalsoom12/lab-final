b2fdc379abf38315f51dcc36262762fcd26c5bb2
umme-kalsoom12 <ummaykulsoom249@gmail.com>
2026-06-08 00:13:32 -0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        chunkSizeWarningLimit: 1600,
+        sourcemap: false, // Turn off sourcemaps to make the build faster
+    }
+});
